0f7b76d1ceba3b5aaab0caa1a56554e872c7cfed
DMax-YT <13312994+DMax-YT@users.noreply.github.com>
2025-05-12 23:49:10 +0300

    introduce `YANDEX_GEOCODER_API_KEY` environment varialbe

--- backend/src/schema.ts
+++ backend/src/schema.ts
@@ -5,4 +5,5 @@ export const EnvironmentSchema = Type.Object({
   PORT: Type.Number({ default: 3000 }),
   JWT_SECRET: Type.String(),
   YANDEX_SUGGEST_API_KEY: Type.String(),
+  YANDEX_GEOCODER_API_KEY: Type.String(),
 });
