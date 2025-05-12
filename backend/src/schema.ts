ef7d335879dea7aaf8bbf2455c88d9a5aca636cb
DMax-YT <13312994+DMax-YT@users.noreply.github.com>
2025-05-12 23:32:41 +0300

    introduce `YANDEX_SUGGEST_API_KEY` environment variable

--- backend/src/schema.ts
+++ backend/src/schema.ts
@@ -4,4 +4,5 @@ export const EnvironmentSchema = Type.Object({
   DATABASE_CONNECTION: Type.String(),
   PORT: Type.Number({ default: 3000 }),
   JWT_SECRET: Type.String(),
+  YANDEX_SUGGEST_API_KEY: Type.String(),
 });
