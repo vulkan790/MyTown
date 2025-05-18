68652cdf24deeba4ff3490de05d9c7b11ab8f65e
VulkanGO <114285945+vulkan790@users.noreply.github.com>
2025-05-18 14:59:49 +0300

    Update main.js

--- vue-demo/src/main.js
+++ vue-demo/src/main.js
@@ -5,8 +5,6 @@ import { VueQueryPlugin } from '@tanstack/vue-query'
 import App from './App.vue'
 import router from './router'
 
-import 'style.css';
-
 const app = createApp(App)
 
 app.use(createPinia())
