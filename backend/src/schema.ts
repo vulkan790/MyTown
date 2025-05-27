c99c9954206733c11c544c3f8df4278c902ec060
DMax-YT <13312994+DMax-YT@users.noreply.github.com>
2025-05-27 11:01:16 +0300

    introduce `DOMAIN` env variable

--- backend/src/schema.ts
+++ backend/src/schema.ts
@@ -3,6 +3,7 @@ import { Type } from '@sinclair/typebox';
 export const EnvironmentSchema = Type.Object({
   DATABASE_CONNECTION: Type.String(),
   PORT: Type.Number({ default: 3000 }),
+  DOMAIN: Type.String(),
   JWT_SECRET: Type.String(),
   YANDEX_SUGGEST_API_KEY: Type.String(),
   YANDEX_GEOCODER_API_KEY: Type.String(),
